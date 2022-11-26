b414afba694c65234ca9193697733403c1c05af3
winni4eva <adams4lyf@gmail.com>
2022-11-26 12:07:06 +0000

    feat: added login request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\NewUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->all());
+
+        if (!Auth::attempt($request->only(['email', 'password'])))
+        {
+            return $this->error('', 'Credentials do not match', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api token of ' . $user->first_name . ' ' . $request->last_name)->plainTextToken
+        ]);
+    }
 
     public function register(NewUserRequest $request)
     {
@@ -26,7 +45,7 @@ class AuthController extends Controller
 
         return $this->success([
             'user' => $user,
-            'token' => $user->createToken('Api token of '. $user->first_name. ' ' . $request->last_name)->plainTextToken
+            'token' => $user->createToken('Api token of ' . $user->first_name . ' ' . $request->last_name)->plainTextToken
         ]);
     }
 }

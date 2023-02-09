0fffbfc682cc2aff9273abf318ad1dc7a6e43a66
Adam Winnipass <adams4lyf@gmail.com>
2023-02-09 03:04:27 +0000

    feat: added account factory

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'amount'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

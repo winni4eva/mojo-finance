c52a150ef5ba59633906506d054c31809454631c
Adam Winnipass <adams4lyf@gmail.com>
2023-03-05 14:34:20 +0000

    feat: added model amount attribute cast

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Traits\AmountTrait;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,5 +21,18 @@ class Account extends Model
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
+    }
+
+    /**
+     * Interact with amount.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function amount(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100,
+        );
     }
 }

913c36dd22fd1c43e6614be6304c3a99e3b13613
Adam Winnipass <adams4lyf@gmail.com>
2023-04-27 20:53:51 +0000

    ch: removed transaction user relationship

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -22,9 +22,4 @@ class Transaction extends Model
     {
         return $this->belongsTo(Account::class);
     }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }

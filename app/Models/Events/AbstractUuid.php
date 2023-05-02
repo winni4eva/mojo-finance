e1f8845d847653c29a2be5fbcca37e4947de9a32
Adam Winnipass <adams4lyf@gmail.com>
2023-05-02 13:58:58 +0000

    feat: refactored abstract uuid from pipelines

--- app/Models/Events/AbstractUuid.php
+++ app/Models/Events/AbstractUuid.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Pipelines;
+namespace App\Models\Events;
 
 use App\Models\Account;
 use Closure;
--- app/Models/Events/SetAccountNumberUuid.php
+++ app/Models/Events/SetAccountNumberUuid.php
@@ -3,7 +3,6 @@
 namespace App\Models\Events;
 
 use App\Models\Account;
-use App\Models\Pipelines\AbstractUuid;
 
 class SetAccountNumberUuid extends AbstractUuid
 {

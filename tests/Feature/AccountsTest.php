91875c48199caf9071a87b4b261aa8fe6aa0d1e0
Adam Winnipass <adams4lyf@gmail.com>
2023-05-07 11:40:06 +0000

    en: fixed failing account create test

--- tests/Feature/AccountsTest.php
+++ tests/Feature/AccountsTest.php
@@ -5,13 +5,10 @@ namespace Tests\Feature;
 use App\Models\Account;
 use App\Models\AccountType;
 use App\Models\User;
-use App\Service\TransactionService;
 use App\Traits\HttpResponseTrait;
 use Database\Seeders\AccountSeeder;
 use Database\Seeders\AccountTypeSeeder;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class AccountsTest extends TestCase
@@ -38,78 +35,48 @@ class AccountsTest extends TestCase
         $response->assertJsonPath('data.1.relationships.account_type.attributes.name', 'checking');
     }
 
-    public function test_authenticated_user_can_get_all_related_accounts()
+    public function test_can_create_user_account()
     {
-        $randomUser = User::factory()->create();
-        Account::factory(count: 3)->create([
-            'user_id' => $randomUser->id,
-        ]);
-        Account::factory(count: 2)->create([
-            'user_id' => $this->user->id,
-        ]);
+        $accountType = AccountType::first();
+        $accountPayload = [
+            'amount' => 1455.54,
+            'account_type' => $accountType->id
+        ];
+        $user = User::factory()->create();
 
-        $response = $this->actingAs($this->user)->get('/api/accounts', ['Accept' => 'application/json']);
+        $response = $this->actingAs($user)->post('/api/v1/accounts', $accountPayload);
 
-        $response->assertStatus(Response::HTTP_OK)
-            ->assertJsonStructure([
-                'data' => [
-                    '*' => [
+        $response->assertCreated();
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'attributes' => [
+                    'amount',
+                    'account_number',
+                    'created_at',
+                    'updated_at',
+                ],
+                'relationships' => [
+                    'user' => [
                         'id',
                         'attributes' => [
-                            'amount',
+                            'first_name',
+                            'other_name',
+                            'last_name',
+                            'email',
                             'created_at',
                             'updated_at',
                         ],
-                        'relationships' => [
-                            'user' => [
-                                'id',
-                                'attributes' => [
-                                    'first_name',
-                                    'other_name',
-                                    'last_name',
-                                    'email',
-                                    'created_at',
-                                    'updated_at',
-                                ],
-                                'relationships',
-                            ],
-                        ],
+                        'relationships',
                     ],
+                    'account_type' => [
+                        'id',
+                        'attributes' => [
+                            'name'
+                        ]
+                    ]
                 ],
-            ])
-            ->assertJsonCount(count: 2, key: 'data');
-    }
-
-    public function test_can_store_user_account()
-    {
-        $accountPayload = ['amount' => 1455.54];
-
-        $response = $this->actingAs($this->user)->post('/api/accounts', $accountPayload, ['Accept' => 'application/json']);
-
-        $response->assertStatus(Response::HTTP_CREATED)
-            ->assertJsonStructure([
-                'data' => [
-                    'id',
-                    'attributes' => [
-                        'amount',
-                        'created_at',
-                        'updated_at',
-                    ],
-                    'relationships' => [
-                        'user' => [
-                            'id',
-                            'attributes' => [
-                                'first_name',
-                                'other_name',
-                                'last_name',
-                                'email',
-                                'created_at',
-                                'updated_at',
-                            ],
-                            'relationships',
-                        ],
-                    ],
-                ],
-            ]);
+            ],
+        ]);
     }
 }

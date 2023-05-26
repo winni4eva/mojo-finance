8f1853805fda3a3d23b12bdd0a39a497900966b1
Adam Winnipass <adams4lyf@gmail.com>
2023-05-26 18:48:00 +0000

    feat: added scheduled transactions test

--- tests/Feature/TransactionsTest.php
+++ tests/Feature/TransactionsTest.php
@@ -3,11 +3,10 @@
 namespace Tests\Feature;
 
 use App\Jobs\ProcessTransaction;
+use App\Jobs\ScheduleTransaction;
 use App\Models\Account;
-use App\Models\ScheduledTransaction;
 use Database\Seeders\AccountSeeder;
 use Database\Seeders\AccountTypeSeeder;
-use Illuminate\Support\Facades\Bus;
 use Tests\FeatureTestCase;
 use Illuminate\Support\Facades\Queue;
 
@@ -33,9 +32,7 @@ class TransactionsTest extends FeatureTestCase
 
         $postData = [
             'credit_account' => $creditAccount->id,
-            'amount' => 10,
-            'schedule' => 0,
-            'period' => '2023-05-26 16:04:00'
+            'amount' => 10
         ];
         Queue::fake();
 
@@ -49,6 +46,33 @@ class TransactionsTest extends FeatureTestCase
         ]);
         
         Queue::assertPushed(ProcessTransaction::class, 1);
-        Queue::assertNotPushed(ScheduledTransaction::class);
+        Queue::assertNotPushed(ScheduleTransaction::class);
+    }
+
+    public function test_transaction_is_scheduled_when_schedule_is_on()
+    {
+        $userAccounts = Account::where('user_id', $this->user->id)->get();
+        $debitAccount = $userAccounts->first();
+        $creditAccount = $userAccounts->get(1);
+
+        $postData = [
+            'credit_account' => $creditAccount->id,
+            'amount' => 10,
+            'schedule' => 1,
+            'period' => '2023-05-26 16:04:00'
+        ];
+        Queue::fake();
+
+        $response = $this->actingAs($this->user)->postJson("/api/v1/accounts/{$debitAccount->id}/transactions", $postData);
+
+        $response->assertCreated();
+        $response->assertJson([
+            'status' => 'Request was successful.',
+            'message' => 'Transaction scheduled successfully',
+            'data' => ''
+        ]);
+        
+        Queue::assertPushed(ScheduleTransaction::class, 1);
+        Queue::assertNotPushed(ProcessTransaction::class);
     }
 }

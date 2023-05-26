e12f9e96c52ee06048be8cf49ce701f150da1166
Adam Winnipass <adams4lyf@gmail.com>
2023-05-26 19:01:23 +0000

    feat: added wrong debit account validation test on transactions

--- tests/Feature/TransactionsTest.php
+++ tests/Feature/TransactionsTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\Jobs\ProcessTransaction;
 use App\Jobs\ScheduleTransaction;
 use App\Models\Account;
+use App\Models\AccountType;
+use App\Models\User;
 use Database\Seeders\AccountSeeder;
 use Database\Seeders\AccountTypeSeeder;
 use Tests\FeatureTestCase;
@@ -74,5 +76,28 @@ class TransactionsTest extends FeatureTestCase
         
         Queue::assertPushed(ScheduleTransaction::class, 1);
         Queue::assertNotPushed(ProcessTransaction::class);
+    }
+
+    public function test_transaction_post_should_return_errors_when_debit_account_doesnt_belong_to_user()
+    {
+        $userAccounts = Account::where('user_id', $this->user->id)->get();
+        $creditAccount = $userAccounts->get(1);
+        $anotherUser = User::factory()->create();
+        $accountType = AccountType::first();
+        $anotherUsersAccount = Account::factory()->create(['account_type_id' => $accountType->id, 'user_id' => $anotherUser->id]);
+
+        $postData = [
+            'credit_account' => $creditAccount->id,
+            'amount' => 1034,
+        ];
+
+        $response = $this->actingAs($this->user)->postJson("/api/v1/accounts/{$anotherUsersAccount->id}/transactions", $postData);
+
+        $response->assertForbidden();
+        $response->assertJson([
+            'status' => 'An error has occurred...',
+            'message' => 'You are not authorized to make this request',
+            'data' => ''
+        ]);
     }
 }

a26ffc330ad22a2e8b00c83db18285e14086eac8
Adam Winnipass <adams4lyf@gmail.com>
2023-05-27 10:14:35 +0000

    refactor: moved transaction user test preparations variales to test class properties

--- tests/Feature/TransactionsTest.php
+++ tests/Feature/TransactionsTest.php
@@ -16,6 +16,12 @@ class TransactionsTest extends FeatureTestCase
 {
     protected $user;
 
+    protected $userAccounts;
+
+    protected $debitAccount;
+
+    protected $creditAccount;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -24,21 +30,21 @@ class TransactionsTest extends FeatureTestCase
             AccountTypeSeeder::class,
             AccountSeeder::class,
         ]);
+        $this->userAccounts = Account::where('user_id', $this->user->id)->get();
+        $this->debitAccount = $this->userAccounts->first();
+        $this->creditAccount = $this->userAccounts->get(1);
     }
 
     public function test_transaction_is_processed_when_schedule_is_off()
     {
-        $userAccounts = Account::where('user_id', $this->user->id)->get();
-        $debitAccount = $userAccounts->first();
-        $creditAccount = $userAccounts->get(1);
-
         $postData = [
-            'credit_account' => $creditAccount->id,
+            'credit_account' => $this->creditAccount->id,
             'amount' => 10,
         ];
         Queue::fake();
 
-        $response = $this->actingAs($this->user)->postJson("/api/v1/accounts/{$debitAccount->id}/transactions", $postData);
+        $response = $this->actingAs($this->user)
+                        ->postJson("/api/v1/accounts/{$this->debitAccount->id}/transactions", $postData);
 
         $response->assertCreated();
         $response->assertJson([
@@ -53,19 +59,16 @@ class TransactionsTest extends FeatureTestCase
 
     public function test_transaction_is_scheduled_when_schedule_is_on()
     {
-        $userAccounts = Account::where('user_id', $this->user->id)->get();
-        $debitAccount = $userAccounts->first();
-        $creditAccount = $userAccounts->get(1);
-
         $postData = [
-            'credit_account' => $creditAccount->id,
+            'credit_account' => $this->creditAccount->id,
             'amount' => 10,
             'schedule' => 1,
             'period' => '2023-05-26 16:04:00',
         ];
         Queue::fake();
 
-        $response = $this->actingAs($this->user)->postJson("/api/v1/accounts/{$debitAccount->id}/transactions", $postData);
+        $response = $this->actingAs($this->user)
+                        ->postJson("/api/v1/accounts/{$this->debitAccount->id}/transactions", $postData);
 
         $response->assertCreated();
         $response->assertJson([
@@ -80,14 +83,12 @@ class TransactionsTest extends FeatureTestCase
 
     public function test_transaction_post_should_return_errors_when_debit_account_doesnt_belong_to_user()
     {
-        $userAccounts = Account::where('user_id', $this->user->id)->get();
-        $creditAccount = $userAccounts->get(1);
         $anotherUser = User::factory()->create();
         $accountType = AccountType::first();
         $anotherUsersAccount = Account::factory()->create(['account_type_id' => $accountType->id, 'user_id' => $anotherUser->id]);
 
         $postData = [
-            'credit_account' => $creditAccount->id,
+            'credit_account' => $this->creditAccount->id,
             'amount' => 1034,
         ];
 
@@ -103,18 +104,14 @@ class TransactionsTest extends FeatureTestCase
 
     public function test_transaction_post_should_return_errors_when_schedule_period_is_not_a_valid_date()
     {
-        $userAccounts = Account::where('user_id', $this->user->id)->get();
-        $debitAccount = $userAccounts->first();
-        $creditAccount = $userAccounts->get(1);
-
         $postData = [
-            'credit_account' => $creditAccount->id,
+            'credit_account' => $this->creditAccount->id,
             'amount' => 1034,
             'schedule' => 1,
             'period' => '&^%$-05-WE 16:04:00',
         ];
 
-        $response = $this->actingAs($this->user)->postJson("/api/v1/accounts/{$debitAccount->id}/transactions", $postData);
+        $response = $this->actingAs($this->user)->postJson("/api/v1/accounts/{$this->debitAccount->id}/transactions", $postData);
 
         $response->assertForbidden();
         $response->assertJson([
@@ -126,16 +123,12 @@ class TransactionsTest extends FeatureTestCase
 
     public function test_transaction_post_should_return_errors_when_amount_is_not_a_number()
     {
-        $userAccounts = Account::where('user_id', $this->user->id)->get();
-        $debitAccount = $userAccounts->first();
-        $creditAccount = $userAccounts->get(1);
-
         $postData = [
-            'credit_account' => $creditAccount->id,
+            'credit_account' => $this->creditAccount->id,
             'amount' => 'QWERTY',
         ];
 
-        $response = $this->actingAs($this->user)->postJson("/api/v1/accounts/{$debitAccount->id}/transactions", $postData);
+        $response = $this->actingAs($this->user)->postJson("/api/v1/accounts/{$this->debitAccount->id}/transactions", $postData);
 
         $response->assertForbidden();
         $response->assertJson([
